2c5a8b3f5f74b9dac6d846ceb92fe69965c3e3ad
Alexey Syrtsov <lexon33@mail.ru>
2018-06-08 19:10:29 +0300

    fifth

--- src/index.js
+++ src/index.js
@@ -33,17 +33,30 @@ function init (){
         .then(()=> {return callAPI('users.get',{name_case:'gen'});
     })
     .then(([me]) => {
-        
-    
         return callAPI('friends.get', {fields:'photo_100'})
     })
-        .then(friends => {
-            console.log(results);
-            const template = document.querySelector('#user-template').textContent;
-            const render = Handlebars.compile(template);
-            const html = render(friends.items);
-            const results = document.querySelector('#results');
-            //results.innerHTML = html;
+        .then(friends => {  
+        //console.log(friends)
+            let container = document.querySelector('.friends');
+            console.log(friends)    
+            for(let i = 0; i<friends.length; i++){
+                console.log(a)
+                let friend = document.createElement('div');
+                let avatarContainer = document.createElement('div');
+                let name = document.createElement('div');
+                let button = document.createElement('div'); 
+                let firstName = friends[i].first_name;
+                let lastName = friends[i].last_name;
+                name.innerHTML = friends[i].first_name + ' ' + friends[i].last_name;
+                let avatarImage = document.createElement('img');
+                avatarImage.setAttribute('src',friends[i].photo_100);
+                avatarContainer.appendChild(avatarImage);
+                friend.appendChild(avatarContainer);
+                friend.appendChild(name);
+                //friend.appendChild(button);
+                container.appendChild(friend);
+                
+        }
         })
 }
 window.onload = init();
