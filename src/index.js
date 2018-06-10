5cf337c67c71dabfdd2ecf9fca69ac941eed6721
Alexey Syrtsov <lexon33@mail.ru>
2018-06-10 21:31:52 +0300

    seventh

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-
-function init (){
-    VK.init({
-        apiId: 6502079
-       });
-    
-    function auth(){
+const myModule = {
+    leftList:undefined,
+    rightList:undefined,
+    allFriends:undefined,
+    leftFriends:undefined,
+    rightFriends:undefined,
+    init: () => {
+        VK.init({
+            apiId: 6502079
+           });
+         
+        myModule.auth()
+            .then(()=> {return myModule.callAPI('users.get',{name_case:'gen'});
+        })
+        .then(([me]) => {
+            return myModule.callAPI('friends.get', {fields:'photo_100'})
+        })
+        .then(friends => {  
+            //console.log(friends)
+            myModule.leftList = document.querySelector('.friends.left');
+            myModule.allFriends = friends.items;
+            console.log(myModule.allFriends)    
+            myModule.render(friends.items, myModule.leftList);
+            //console.log(friends.items)
+            })
+    },
+    auth: () =>{
         return new Promise((resolve, reject)=>{
             VK.Auth.login(data =>{
                 if(data.session) {
@@ -14,11 +34,10 @@ function init (){
                 }
             } ,2);
         });
-    }
-    
-    function callAPI(method, params){
+    },
+    callAPI: (method, params) =>{
         params.v='5.76';
-        
+
         return new Promise((resolve, reject)=>{
             VK.api(method, params, (data)=>{
                 if(data.error){
@@ -28,59 +47,81 @@ function init (){
                 }
             })
         })
-    }
-    auth()
-        .then(()=> {return callAPI('users.get',{name_case:'gen'});
-    })
-    .then(([me]) => {
-        return callAPI('friends.get', {fields:'photo_100'})
-    })
-        .then(friends => {  
-        //console.log(friends)
-            let container = document.querySelector('.friends');
-            console.log(friends)    
-            for(let i = 0; i<friends.items.length; i++){
-                //console.log(a)
-                let friend = document.createElement('div');
-                friend.classList.add("friend");
-                friend.setAttribute('draggable', true)
-                friend.setAttribute('id', 'drop_start');
-                friend.setAttribute('ondragstart','dragStart(event)');
-                function dragStart(e) { 
-                    e.dataTransfer.effectAllowed='move';
-                    e.dataTransfer.setData("Text", e.target.getAttribute('id'));
-                    return true;
-                    }
-                friend.ondragenter = function dragEnter(e) {
-                    event.preventDefault();
-                    return true;
-                    }
-                function dragDrop(e) {
-                    var data = e.dataTransfer.getData("text/plain");
-                    e.target.appendChild(document.getElementById(data));
-                    ev.stopPropagation();
-                    }  
-                function dragOver(e) {
-                    event.preventDefault();
-                }
-                //console.log(friend);
-                let avatarContainer = document.createElement('div');
-                let name = document.createElement('div');
-                name.classList.add("name");
-                let button = document.createElement('div'); 
-                let firstName = friends.items[i].first_name;
-                let lastName = friends.items[i].last_name;
-                name.innerHTML = friends.items[i].first_name + ' ' + friends.items[i].last_name;
-                let avatarImage = document.createElement('img');
-                avatarImage.classList.add("avatar_img");
-                avatarImage.setAttribute('src',friends.items[i].photo_100);
-                avatarContainer.appendChild(avatarImage);
-                friend.appendChild(avatarContainer);
-                friend.appendChild(name);
-                //friend.appendChild(button);
-                container.appendChild(friend);
-                //const newFriend = `<div><div> <img src="${friends.items[i].photo_100}> </div><div></div><div></div></div>`
+    },
+    isMatching:(full, chunk) => {
+        //console.log(chunk)
+        if(full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1){
+          return true;
+        } else {
+          return false;
         }
-        })
+      },
+      filter:() =>{
+            const filterInput = document.querySelector('#search_left');
+            filterInput.addEventListener('keyup', function() {
+            let result = [];
+            console.log(result);
+                for( let i = 0; i <myModule.allFriends.length; i++){
+                    let nameFilter = 'myModule.allFriends[i].first_name' + ' ' + 'myModule.allFriends[i].last_name';
+                    //console.log(nameFilter);
+                    if(myModule.isMatching(nameFilter, event.target.value)){
+                        result.push(myModule.allFriends[i]);
+                    }
+                }
+            myModule.render(result).myModule.leftList;  
+            }
+        )},
+    render: (array, container) =>{
+        container.innerHTML = '';
+        for(let i = 0; i<array.length; i++){
+        let friend = document.createElement('div');
+        friend.classList.add("friend");
+        friend.setAttribute('draggable', true)
+        friend.setAttribute('id',array[i].id);
+        friend.setAttribute('ondragstart','dragStart(event)');
+        console.log(friend);
+        let avatarContainer = document.createElement('div');
+        let name = document.createElement('div');
+        name.classList.add("name");
+        let button = document.createElement('div'); 
+        let firstName = array[i].first_name;
+        let lastName = array[i].last_name;
+        name.innerHTML = array[i].first_name + ' ' + array[i].last_name;
+        let avatarImage = document.createElement('img');
+        avatarImage.classList.add("avatar_img");
+        avatarImage.setAttribute('draggable', false);
+        avatarImage.setAttribute('src',array[i].photo_100);
+        avatarContainer.appendChild(avatarImage);
+        friend.appendChild(avatarContainer);
+        friend.appendChild(name);
+        //friend.appendChild(button);
+        container.appendChild(friend);
+        //const newFriend = `<div><div> <img src="${friends.items[i].photo_100}> </div><div></div><div></div></div>`
+    }
+    }
 }
-window.onload = init();
+window.onload = myModule.init();
+
+window.dragStart = function(e) { 
+    e.dataTransfer.effectAllowed='move';
+    e.dataTransfer.setData("Text", e.target.getAttribute('id'));
+    return true;
+    }
+window.dragEnter = function(e) {
+    event.preventDefault();
+    return true;
+    }
+window.dragDrop = function(e) {
+    let data = e.dataTransfer.getData("text/plain");
+    const list = e.target.closest ('.friends');
+    list.appendChild(document.getElementById(data));
+    e.stopPropagation();
+    }  
+window.dragOver = function(e) {
+    event.preventDefault();
+}
+
+
+//прочитать про closest 
+// ООП
+//обратботчик на инпут перед рендером
